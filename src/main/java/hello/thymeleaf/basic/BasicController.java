448cd080ec3eedf89246c3d72e96cb288a2c30f8
iamminho <minho100227@kakao.com>
2023-05-07 23:53:44 +0900

    연산

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -82,6 +82,13 @@ public class BasicController {
         return "basic/literal";
     }
 
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
     @Data
     public class User {
         String username;
